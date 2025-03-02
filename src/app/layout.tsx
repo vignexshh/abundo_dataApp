2de4796d23eeed01264f7e20625bc3cc1b8f2940
vigneshtd <vigneshtd111@gmail.com>
2025-03-02 18:45:02 +0530

    Revert "added root layouts"
    This reverts commit 39ddeefca932129e2b1da0331b5503a10c56d333.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import React from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ConfigProvider } from 'antd';
-import CustomLayout from "@/components/CustomLayout";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import '@ant-design/v5-patch-for-react-19';
+
 
 
 const geistSans = Geist({
@@ -16,36 +16,21 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata: Metadata = {
   title: "MedicalHunt NextGen",
   description: "A application to analyse and tabulate NEET scores across india for better preperations",
 };
 
+
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <AntdRegistry>
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: '#52c41a',
-              colorSuccess: '#52c41a', // Success color
-              colorWarning: '#faad14', // Warning color
-              colorError: '#f5222d', // Error color
-              colorInfo: '#1890ff', // Info color
-              colorTextBase: '#000000', // Base text color
-              colorBgBase: '#ffffff',
-            },
-          }}
-        >
-
-          
-          <CustomLayout>
-            {children}
-          </CustomLayout>
-        </ConfigProvider>
-       </AntdRegistry>
+        {/* theme provider issue arises here  */}
+        <AntdRegistry>
+          {children}
+        </AntdRegistry>
       </body>
     </html>
   );
